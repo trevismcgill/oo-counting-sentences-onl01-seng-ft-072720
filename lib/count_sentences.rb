b18d48244119a9883f0a49f6f2f3f8994a567ed1
Trevis McGill <trevismcgill25@gmail.com>
2020-08-05 00:33:20 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # binding.pry
+    split_sent = self.split(/[.?!...]/).reject(&:empty?)
+    split_sent.length
+    # binding.pry
   end
 end
